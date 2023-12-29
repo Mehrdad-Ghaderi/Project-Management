0a8c7cf498ce51e24308fe78a1cd8451476febd9
Mehrdad Ghaderi <mehrdadghaderi83@gmail.com>
2023-12-29 23:52:00 +0330

    home.html was implemented HomeController class was implemented

---
+++ src/main/java/com/example/projectmanagement/controllers/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.projectmanagement.controllers;
+
+import com.example.projectmanagement.dao.ProjectRepository;
+import com.example.projectmanagement.entities.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @GetMapping("/home")
+    public String displayHome(Model model) {
+        List<Project> projectList = projectRepository.findAll();
+        model.addAttribute("projectList", projectList);
+        return "home";
+    }
+}
