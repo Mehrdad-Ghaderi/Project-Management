c092c25e31ff693fb5326ffe3df650eafa4a7982
Mehrdad Ghaderi <mehrdadghaderi83@gmail.com>
2023-12-31 20:34:40 +0330

    home.html was modified

--- src/main/java/com/example/projectmanagement/controllers/HomeController.java
+++ src/main/java/com/example/projectmanagement/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.projectmanagement.controllers;
 
+import com.example.projectmanagement.dao.EmployeeRepository;
 import com.example.projectmanagement.dao.ProjectRepository;
 import com.example.projectmanagement.entities.Project;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,16 @@ public class HomeController {
     @Autowired
     ProjectRepository projectRepository;
 
+    @Autowired
+    EmployeeRepository employeeRepository;
+
     @GetMapping("/")
     public String displayHome(Model model) {
         List<Project> projectList = projectRepository.findAll();
         model.addAttribute("projectList", projectList);
+
+        var employees = employeeRepository.findAll();
+        model.addAttribute("employees", employees);
         return "main/home";
     }
 }
