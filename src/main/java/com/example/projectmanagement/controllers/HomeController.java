f82d807b80814305c1443c80cc21c9cb67667d92
Mehrdad Ghaderi <mehrdadghaderi83@gmail.com>
2024-01-21 23:32:01 +0330

    version was added to application.properties

--- src/main/java/com/example/projectmanagement/controllers/HomeController.java
+++ src/main/java/com/example/projectmanagement/controllers/HomeController.java
@@ -6,6 +6,7 @@ import com.example.projectmanagement.entities.Project;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,19 +18,25 @@ import java.util.Map;
 @Controller
 public class HomeController {
 
+    //This "version" is read from the application.properties file
+    @Value("${version}")
+    private String ver;
+
     @Autowired
     ProjectRepository projectRepository;
+
 
     @Autowired
     EmployeeRepository employeeRepository;
 
     @GetMapping("/home")
     public String displayHome(Model model) throws JsonProcessingException {
-        Map<String, Object> map = new HashMap<>();
+        model.addAttribute("versionNumber", ver);
 
         List<Project> projectList = projectRepository.findAll();
         model.addAttribute("projectList", projectList);
 
+        Map<String, Object> map = new HashMap<>();
         var chartData = projectRepository.getProjectStatus();
         //converting chartData to JSON for JS
         ObjectMapper objectMapper = new ObjectMapper();
