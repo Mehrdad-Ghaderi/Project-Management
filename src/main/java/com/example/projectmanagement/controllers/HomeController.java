28d12c5309aba72aa192dced848a02d627ec6dfc
Mehrdad Ghaderi <mehrdadghaderi83@gmail.com>
2024-01-25 14:45:07 +0330

    MySql database was implemented and works fine

--- src/main/java/com/example/projectmanagement/controllers/HomeController.java
+++ src/main/java/com/example/projectmanagement/controllers/HomeController.java
@@ -39,14 +39,12 @@ public class HomeController {
         Map<String, Object> map = new HashMap<>();
         var chartData = projectRepository.getProjectStatus();
 
-        if (!chartData.isEmpty()) {
-            //converting chartData to JSON for JS
-            ObjectMapper objectMapper = new ObjectMapper();
-            String jsonString = objectMapper.writeValueAsString(chartData);
-            //the former method creates this: {["NOTSTARTED",1], ["COMPLETED", 2], ["INPROGRESS", 4]}
+        //converting chartData to JSON for JS
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(chartData);
+        //the former method creates this: {["NOTSTARTED",1], ["COMPLETED", 2], ["INPROGRESS", 4]}
 
-            model.addAttribute("projectStatusCount", jsonString);
-        }
+        model.addAttribute("projectStatusCount", jsonString);
 
         var employeeProjectsCount = employeeRepository.getEmployeeProjectsCount();
         model.addAttribute("employeesProjectCount", employeeProjectsCount);
