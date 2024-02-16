83ef135f0bd4648cfc142d91a67d8408b45148f9
Mehrdad Ghaderi <mehrdadghaderi83@gmail.com>
2024-02-16 17:23:13 +0330

    ApplicationLoggerAspect.java was modified logging was added to application-dev.properties

--- src/main/java/com/example/projectmanagement/logging/ApplicationLoggerAspect.java
+++ src/main/java/com/example/projectmanagement/logging/ApplicationLoggerAspect.java
@@ -1,11 +1,15 @@
 package com.example.projectmanagement.logging;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 @Aspect
@@ -13,15 +17,20 @@ public class ApplicationLoggerAspect {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Pointcut("within(com.example.projectmanagement.controllers..*) " +
-            "|| within(com.example.projectmanagement.dao..*)")
+    @Pointcut("within(com.example.projectmanagement.controllers..*)")
     //pointcut defines WHERE that logging should happen
     public void definedPackagedPointcuts() {
 
     }
 
     @After("definedPackagedPointcuts()") //this what is done
-    public void log() {
-        logger.debug("*-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-*");
+    public void logAfter(JoinPoint joinPoint) {
+        logger.debug("\n \n \n");
+        logger.debug("********** Before method execution *********\n {}.{} () with arguments[s] = {}",
+                joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(),
+                Arrays.toString(joinPoint.getArgs()));
+
+        logger.debug("*********************************************** \n \n \n");
     }
 }
