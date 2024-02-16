91e0b23cbc7608b44f2f0e89e9ec5993dd6350d3
Mehrdad Ghaderi <mehrdadghaderi83@gmail.com>
2024-02-17 00:30:20 +0330

    security dependency was added to pom.xml

--- src/main/java/com/example/projectmanagement/ProjectManagementApplication.java
+++ src/main/java/com/example/projectmanagement/ProjectManagementApplication.java
@@ -3,10 +3,10 @@ package com.example.projectmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ProjectManagementApplication {
-
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectManagementApplication.class, args);
--- src/main/java/com/example/projectmanagement/entities/Employee.java
+++ src/main/java/com/example/projectmanagement/entities/Employee.java
@@ -1,7 +1,6 @@
 package com.example.projectmanagement.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import java.util.List;
 
 @Entity
